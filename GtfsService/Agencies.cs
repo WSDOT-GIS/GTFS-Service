853907840a9dfa406e42a57baf9f1711f52b26a8
JeffJacobson <jeffrey.m.jacobson@github.fake>
2013-09-03 16:45:58 -0700

    Added Agencies request.

---
+++ GtfsService/Agencies.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using ServiceStack.ServiceHost;
+using ServiceStack.ServiceInterface;
+using System.Configuration;
+using System.Net;
+using System.IO;
+
+namespace GtfsService
+{
+	[Route("/agencies")]
+	public class Agencies
+	{
+	}
+
+	public class AgenciesService: Service
+	{
+		/// <summary>
+		/// Redirects to the gtfs-data-exchange.com agencies list.
+		/// </summary>
+		/// <param name="request"></param>
+		public void Any(Agencies request)
+		{
+			var url = ConfigurationManager.AppSettings["gtfs-url"].TrimEnd('/') + "/api/agencies";
+			base.Response.RedirectToUrl(url);
+		}
+	}
+
+}
---
+++ GtfsService/Global.asax.cs
@@ -0,0 +1,65 @@
+﻿using ServiceStack.CacheAccess;
+using ServiceStack.CacheAccess.Providers;
+using ServiceStack.WebHost.Endpoints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace GtfsService
+{
+	public class Global : System.Web.HttpApplication
+	{
+		public class AppHost: AppHostBase
+		{
+			public AppHost(): base("GTFS Services", Assembly.GetExecutingAssembly())
+			{
+
+			}
+
+			public override void Configure(Funq.Container container)
+			{
+				container.Register<ICacheClient>(new MemoryCacheClient());
+			}
+		}
+
+
+		protected void Application_Start(object sender, EventArgs e)
+		{
+			new AppHost().Init();
+		}
+
+		protected void Session_Start(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void Application_BeginRequest(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void Application_AuthenticateRequest(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void Application_Error(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void Session_End(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void Application_End(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
