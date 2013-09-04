68992520a2f682d4a565f565e6afdadecb331239
JeffJacobson <Jeffrey.M.Jacobson@gmail.com>
2013-09-04 15:28:06 -0700

    Responses are now cached on the server for one day.

--- GtfsService/Feed.cs
+++ GtfsService/Feed.cs
@@ -22,7 +22,7 @@ namespace GtfsService
 	/// </summary>
 	public class FeedService : Service
 	{
-		public GtfsFeed Any(Feed request)
+		public object Any(Feed request)
 		{
 			// Test to make sure that an agency was specified.
 			if (string.IsNullOrWhiteSpace(request.agency))
@@ -30,23 +30,27 @@ namespace GtfsService
 				throw new ArgumentNullException("Agency ID not provided.");
 			}
 
-			// Construct the gtfs-data-exchange URL for the specified agency.
-			// E.g., http://www.gtfs-data-exchange.com/agency/intercity-transit/latest.zip
-			string url = string.Format("{0}/agency/{1}/latest.zip", ConfigurationManager.AppSettings["gtfs-url"].TrimEnd('/'), request.agency);
-
-			HttpWebRequest zipRequest = WebRequest.CreateHttp(url);
-
-			GtfsFeed gtfs;
-
-			using (var response = (HttpWebResponse)zipRequest.GetResponse())
+			return base.RequestContext.ToOptimizedResultUsingCache(base.Cache, request.agency, new TimeSpan(1, 0, 0, 0), () =>
 			{
-				using (var stream = response.GetResponseStream())
-				{
-					gtfs = stream.ReadGtfs();
-				}
-			}
+				// Construct the gtfs-data-exchange URL for the specified agency.
+				// E.g., http://www.gtfs-data-exchange.com/agency/intercity-transit/latest.zip
+				string url = string.Format("{0}/agency/{1}/latest.zip", ConfigurationManager.AppSettings["gtfs-url"].TrimEnd('/'), request.agency);
 
-			return gtfs;
+				HttpWebRequest zipRequest = WebRequest.CreateHttp(url);
+
+				GtfsFeed gtfs;
+
+				using (var response = (HttpWebResponse)zipRequest.GetResponse())
+				{
+					using (var stream = response.GetResponseStream())
+					{
+						gtfs = stream.ReadGtfs();
+					}
+				}
+
+				return gtfs;
+			});
+
 		}
 	}
 }
