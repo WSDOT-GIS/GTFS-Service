aaa6647c52c2109e859161ad034463cad5a7e1d1
JeffJacobson <Jeffrey.M.Jacobson@gmail.com>
2013-09-04 09:57:50 -0700

    Added Feed service.

---
+++ GtfsService/Feed.cs
@@ -0,0 +1,49 @@
+﻿using ServiceStack.ServiceHost;
+using ServiceStack.ServiceInterface;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Web;
+using Wsdot.Gtfs.Contract;
+using Wsdot.Gtfs.IO;
+
+namespace GtfsService
+{
+	[Route("/feed/{agency}")]
+	public class Feed
+	{
+		public string agency { get; set; }
+	}
+
+	public class FeedService : Service
+	{
+		public GtfsFeed Any(Feed request)
+		{
+			// Test to make sure that an agency was specified.
+			if (string.IsNullOrWhiteSpace(request.agency))
+			{
+				throw new ArgumentNullException("Agency ID not provided.");
+			}
+
+			// Construct the gtfs-data-exchange URL for the specified agency.
+			// E.g., http://www.gtfs-data-exchange.com/agency/intercity-transit/latest.zip
+			string url = string.Format("{0}/agency/{1}/latest.zip", ConfigurationManager.AppSettings["gtfs-url"].TrimEnd('/'), request.agency);
+
+			HttpWebRequest zipRequest = WebRequest.CreateHttp(url);
+
+			GtfsFeed gtfs;
+
+			using (var response = (HttpWebResponse)zipRequest.GetResponse())
+			{
+				using (var stream = response.GetResponseStream())
+				{
+					gtfs = stream.ReadGtfs();
+				}
+			}
+
+			return gtfs;
+		}
+	}
+}
