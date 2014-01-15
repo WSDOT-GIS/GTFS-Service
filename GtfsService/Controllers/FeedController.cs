2333d76fdea29d53f254f65f1d2aff10f086637a
JeffJacobson <JeffJacobson@github.fake>
2014-01-14 17:07:30 -0800

    Migrated from ServiceStack to WebAPI.
    Removed references to ServiceStack.
    
    Created FeedController.
    
    Added AgencyController
    
    Deleted unused files
    
    Modified FeedController.Get
    
    Previously requested GTFS feeds are now stored in memory.
    
    Updated GTFS.NET submodule

---
+++ GtfsService/Controllers/FeedController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Wsdot.Gtfs.Contract;
+using Wsdot.Gtfs.IO;
+
+namespace GtfsService.Controllers
+{
+	public class FeedController : ApiController
+	{
+		FeedManager _feedManager = FeedManager.GetInstance();
+
+		[Route("api/feed/{agency}")]
+		public GtfsFeed Get(string agency)
+		{
+			return _feedManager[agency];
+			////// Construct the gtfs-data-exchange URL for the specified agency.
+			////// E.g., http://www.gtfs-data-exchange.com/agency/intercity-transit/latest.zip
+			////string url = string.Format("{0}/agency/{1}/latest.zip", ConfigurationManager.AppSettings["gtfs-url"].TrimEnd('/'), agency);
+			////var client = new HttpClient();
+			////HttpResponseMessage output = null;
+			////var task = client.GetAsync(url).ContinueWith((t) => {
+			////	if (!t.Result.IsSuccessStatusCode)
+			////	{
+			////		output = Request.CreateErrorResponse(t.Result.StatusCode, t.Result.ReasonPhrase);
+			////	}
+			////	else
+			////	{
+			////		GtfsFeed feed = null;
+			////		var response = t.Result;
+			////		var streamTask = response.Content.ReadAsStreamAsync().ContinueWith((st) =>
+			////		{
+			////			feed = GtfsReader.ReadGtfs(st.Result);
+			////			output = Request.CreateResponse<GtfsFeed>(HttpStatusCode.OK, feed);
+			////			output.Headers.ETag = response.Headers.ETag;
+			////			output.Headers.Date = response.Headers.Date;
+			////			output.Headers.CacheControl = new System.Net.Http.Headers.CacheControlHeaderValue
+			////			{
+			////				Public = true,
+			////				MaxAge = default(TimeSpan?)
+			////			};
+			////		});
+			////		streamTask.Wait();
+			////	}
+			////});
+			////task.Wait();
+			////return output;
+		}
+	}
+}
--- GtfsService/Global.asax.cs
+++ GtfsService/Global.asax.cs
@@ -1,38 +1,23 @@
-﻿using ServiceStack.CacheAccess;
-using ServiceStack.CacheAccess.Providers;
-using ServiceStack.Text;
-using ServiceStack.WebHost.Endpoints;
-using System;
+﻿using System;
 using System.Reflection;
+using System.Web.Http;
 using Wsdot.Gtfs.Contract;
 
 namespace GtfsService
 {
 	public class Global : System.Web.HttpApplication
 	{
-		public class AppHost: AppHostBase
-		{
-			public AppHost(): base("GTFS Services", Assembly.GetExecutingAssembly())
-			{
-				JsConfig.TimeSpanHandler = JsonTimeSpanHandler.StandardFormat;
-				JsConfig.TreatEnumAsInteger = true;
-			}
-
-			public override void Configure(Funq.Container container)
-			{
-				container.Register<ICacheClient>(new MemoryCacheClient());
-			}
-		}
-
 
 		protected void Application_Start(object sender, EventArgs e)
 		{
-			new AppHost().Init();
+			var config = GlobalConfiguration.Configuration;
+			config.MapHttpAttributeRoutes();
+			config.EnsureInitialized();
 		}
 
 		protected void Session_Start(object sender, EventArgs e)
 		{
-
+			
 		}
 
 		protected void Application_BeginRequest(object sender, EventArgs e)
