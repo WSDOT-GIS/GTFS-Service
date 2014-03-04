8db150550f18f3b714f5cecb7eab1810475739f9
JeffJacobson <JeffJacobson@users.noreply.github.com>
2014-03-03 17:32:37 -0800

    Progress meter now shows progress instead of being indeterminite.

--- GtfsService/Scripts/index.js
+++ GtfsService/Scripts/index.js
@@ -41,24 +41,40 @@
 
 	// Setup agency submission.
 	document.forms.agencyForm.onsubmit = function () {
-		var progress = document.getElementById("agencyProgress");
-
-		var request = new XMLHttpRequest();
-		request.responseType = "json";
-		request.addEventListener("loadstart", function () {
-			progress.hidden = false;
-		});
-		request.addEventListener("progress", function (e) {
-			progress.max = e.total;
-			progress.value = e.position;
+		/**
+		 * @param {XMLHttpRequestProgressEvent} e
+		 */
+		function updateProgress(e) {
+			if (progress.hidden) {
+				progress.hidden = false;
+			}
+			if (e.lengthComputable) {
+				progress.max = 1;
+				progress.value = e.position / e.total;
+			} else {
+				progress.max = 1;
+				progress.value = 0;
+			}
 			console.log("progress event", e);
-		});
-		request.addEventListener("loadend", function (e) {
+		}
+
+		/**
+		 * @param {XMLHttpRequestProgressEvent} e
+		 */
+		function handleResponse(e) {
 			var gtfs;
 			gtfs = e.target.response;
 			console.log("gtfs", gtfs);
 			progress.hidden = true;
-		});
+		}
+
+		var progress = document.getElementById("agencyProgress");
+
+		var request = new XMLHttpRequest();
+		request.responseType = "json";
+		request.addEventListener("loadstart", updateProgress);
+		request.addEventListener("progress", updateProgress);
+		request.addEventListener("loadend", handleResponse);
 		request.open("GET", ["api/feed/", document.getElementById("agencySelect").value].join(""));
 		request.send();
 
